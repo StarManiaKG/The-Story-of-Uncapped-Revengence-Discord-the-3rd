3ddf107a9e47aae0d14dad00da25c54606e3c587
Louis <louis_foy@yahoo.co.uk>
2019-12-11 23:27:55 +0000

    Final modifications before removing recording stuff

--- src/d_netcmd.h
+++ src/d_netcmd.h
@@ -109,6 +109,10 @@ extern consvar_t cv_maxping;
 extern consvar_t cv_pingtimeout;
 extern consvar_t cv_showping;
 
+extern consvar_t cv_simulate;
+extern consvar_t cv_simulatetics;
+extern consvar_t cv_netdelay;
+extern consvar_t cv_netjitter;
 
 extern consvar_t cv_skipmapcheck;
 
