8aee17b6910aa67381f9a60b8359fb7c7dfceee4
Louis <louis_foy@yahoo.co.uk>
2019-12-22 21:54:03 +0000

    Refined steadyplayer delayed rings

--- src/p_user.c
+++ src/p_user.c
@@ -4198,7 +4198,7 @@ firenormal:
 	{
 		if (mo->flags & MF_MISSILE && mo->flags2 & MF2_RAILRING)
 		{
-			if (cv_netslingdelay.value && issimulation && (tic_t)cv_netsteadyplayers.value > simtic - gametic && mo->target == players[consoleplayer].mo)
+			if (cv_netslingdelay.value && issimulation && (tic_t)cv_netsteadyplayers.value >= targetsimtic - simtic && mo->target == players[consoleplayer].mo)
 				// don't fire it yet
 				return;
 
