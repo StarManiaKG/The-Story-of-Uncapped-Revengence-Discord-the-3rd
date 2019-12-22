837b91158d8dff61e7b7e31555128dbeca1d320d
Louis <louis_foy@yahoo.co.uk>
2019-12-22 18:40:43 +0000

    Stability fixes for steadyplayers Rail rings can now be delayed

--- src/doomstat.h
+++ src/doomstat.h
@@ -539,6 +539,7 @@ extern UINT32 timesBeatenUltimate;
 
 extern tic_t gametic;
 extern tic_t simtic; // current tic at simulation, if applicable (if no simulation happened, this=gametic)
+extern tic_t targetsimtic; // the target sim tic, if applicable
 extern tic_t smoothedTic; // current game tic when smoothing is enabled (\todo: could cause problems if the game objects care about gametic)
 #define localgametic leveltime
 
