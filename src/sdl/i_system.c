4d39af181a615853fef6190395d0fbd29df8487f
Louis <louis_foy@yahoo.co.uk>
2019-12-23 00:27:30 +0000

    More Linux fixes??

--- src/sdl/i_system.c
+++ src/sdl/i_system.c
@@ -2021,14 +2021,14 @@ ticcmd_t *I_BaseTiccmd2(void)
 	return &emptycmd2;
 }
 
+static int lastTimeFudge = -1;
+extern consvar_t cv_timefudge;
+
 #if defined (_WIN32)
 static HMODULE winmm = NULL;
 static DWORD starttickcount = 0; // hack for win2k time bug
 static p_timeGetTime pfntimeGetTime = NULL;
 static LARGE_INTEGER basetime = { {0, 0} };
-static int lastTimeFudge = -1;
-
-extern consvar_t cv_timefudge;
 
 // ---------
 // I_GetTime
@@ -2175,6 +2175,11 @@ tic_t I_GetTime (void)
 	ticks = (ticks/1000);
 
 	return (tic_t)ticks;
+}
+
+UINT64 I_GetTimeUs(void)
+{
+	return (SDL_GetTicks() - basetime) * 1000;
 }
 #endif
 
