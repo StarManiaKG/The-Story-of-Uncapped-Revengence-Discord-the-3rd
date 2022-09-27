c7dcb66211dbf41295c19ce2d6fa13e82ac2e63a
Star <89750877+StarManiaKG@users.noreply.github.com>
2022-09-27 12:07:42 -0400

    maybe this'll let us connect to friends now

--- src/doomdef.h
+++ src/doomdef.h
@@ -122,7 +122,7 @@ extern char logfilename[1024];
 #endif
 
 /* A mod name to further distinguish versions. */
-#define SRB2APPLICATION "Sonic Robo Blast 2"
+#define SRB2APPLICATION "SRB2"
 
 //#define DEVELOP // Disable this for release builds to remove excessive cheat commands and enable MD5 checking and stuff, all in one go. :3
 #ifdef DEVELOP
