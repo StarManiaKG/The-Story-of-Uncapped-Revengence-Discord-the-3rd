c27fc3069a121eaf376890705bcd091a4a43692b
Star <89750877+StarManiaKG@users.noreply.github.com>
2022-09-27 17:11:29 -0400

    maybe this'll work

--- src/m_menu.c
+++ src/m_menu.c
@@ -2601,7 +2601,7 @@ void Moviemode_option_Onchange(void)
 void Discordcustomstatus_option_Onchange(void)
 {
 	DRPC_UpdatePresence();
-	OP_DiscordOptionsMenu[&OP_CustomStatusDef].status =
+	OP_DiscordOptionsMenu[7].status =
 		(cv_discordshowonstatus.value == 7 ? IT_CVAR|IT_STRING|IT_CV_STRING : IT_DISABLED);
 }
 #endif
