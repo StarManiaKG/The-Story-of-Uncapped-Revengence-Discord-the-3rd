f3d199a7cd1b351ecad2dad12da8cfefce949d58
Star <89750877+StarManiaKG@users.noreply.github.com>
2022-09-27 19:49:41 -0400

    i don't even know if the bot stuff works i'm just working on the menu lol

--- src/m_menu.c
+++ src/m_menu.c
@@ -2593,10 +2593,10 @@ void Discordcustomstatus_option_Onchange(void)
 {
 	DRPC_UpdatePresence();
 	
-	OP_DiscordOptionsMenu[7].status =
+	OP_DiscordOptionsMenu[8].status =
 		(cv_discordshowonstatus.value == 7 ? IT_CVAR|IT_STRING|IT_CV_STRING : IT_DISABLED);
 	
-	OP_DiscordOptionsMenu[8].status =
+	OP_DiscordOptionsMenu[9].status =
 		(cv_discordshowonstatus.value == 7 ? IT_CVAR|IT_STRING|IT_CV_STRING : IT_DISABLED);
 }
 #endif
