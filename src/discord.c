6bfff2216959fd1f8a3670a132d94f93d5cec6e1
Star <89750877+StarManiaKG@users.noreply.github.com>
2022-09-28 15:47:39 -0400

    no

--- src/discord.c
+++ src/discord.c
@@ -735,7 +735,7 @@ void DRPC_UpdatePresence(void)
 				//Easier way to check for supported characters
 				int character = (strcmp(skins[players[consoleplayer].skin].name, skins[players[consoleplayer].skin].name));
 
-				CONS_Printf(M_GetText(players[consoleplayer].mo->skin));
+				CONS_Printf(M_GetText(baseSkins[players[consoleplayer].mo->skin]));
 
 				//// No Bots ////
 				if (!players[1].bot || netgame)
