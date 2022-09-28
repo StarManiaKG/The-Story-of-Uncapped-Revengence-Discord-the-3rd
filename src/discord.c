7057995676ebf7f261a688fcade44a254300421c
Star <89750877+StarManiaKG@users.noreply.github.com>
2022-09-28 15:22:55 -0400

    amy fang metalsonic

--- src/discord.c
+++ src/discord.c
@@ -735,7 +735,7 @@ void DRPC_UpdatePresence(void)
 				//Easier way to check for supported characters
 				int character = (strcmp(skins[players[consoleplayer].skin].name, skins[players[consoleplayer].skin].name));
 
-				if baseSkins[] == skins[players[consoleplayer].skin].name
+				if (baseSkins[] == skins[players[consoleplayer].skin].name)
 					CONS_Printf(M_GetText(skins[players[consoleplayer].skin].name));
 
 				//// No Bots ////
