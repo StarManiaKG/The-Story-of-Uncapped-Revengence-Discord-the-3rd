4d79bde4f28ae4ec9ada396aabf8d3e8209d4c63
Star <89750877+StarManiaKG@users.noreply.github.com>
2022-09-27 21:19:01 -0400

    hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm

--- src/discord.c
+++ src/discord.c
@@ -767,11 +767,11 @@ void DRPC_UpdatePresence(void)
 					if (!players[2].bot)
 					{
 						// Character images
-						if ((strcmp(skins[players[consoleplayer].skin].name, "sonic") && (strcmp(((skin_t *)players[1].bot)->name, "tails"))))
+						if ((strcmp(skins[players[consoleplayer].skin].name, "sonic") && (strcmp(((skin_t *)players[1].mo->skin)->name, "tails"))))
 							CONS_Printf(M_GetText("sonic"));
 							snprintf(charimg, 15, "charsonictails");
 						
-						snprintf(charname, 28, "Playing As: %s & %s", skins[players[consoleplayer].skin].name, skins[players[1].skin].realname);
+						snprintf(charname, 28, "Playing As: %s & %s", skins[players[consoleplayer].skin].realname, skins[players[1].skin].realname);
 					}
 					////Multiple Bots?
 					else
