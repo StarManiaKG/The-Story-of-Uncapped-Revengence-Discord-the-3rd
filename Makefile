8cae06f9b9bd19c08c01b568ea9eb02aef7d1b8b
StarManiaKG <DraytonM98@outlook.com>
2023-03-29 09:39:55 -0400

    2.1.25 Starting Stuff

--- Makefile
+++
@@ -1,8 +0,0 @@
-ifdef SILENT
-MAKEFLAGS+=--no-print-directory
-endif
-
-all :
-
-% ::
-	@$(MAKE) -C src $(MAKECMDGOALS)
--- src/sdl/macosx/mac_resources.h
+++
@@ -1,5 +0,0 @@
-#ifndef __MAC_RESOURCES_H__
-#define __MAC_RESOURCES_H__
-
-void OSX_GetResourcesPath(char * buffer);
-#endif
