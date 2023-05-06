26be6039ccd6d57c2bf65b062a84bf7cf7dc727c
StarManiaKG <DraytonM98@outlook.com>
2023-05-06 15:38:11 -0400

    hi hi bitten

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -48,7 +48,6 @@
 #include "lua_libs.h"
 #include "md5.h"
 #include "m_perfstats.h"
-#include "i_time.h"
 
 // aaaaaa
 #include "i_joy.h"
@@ -319,7 +318,7 @@ static void D_FreeTextcmd(tic_t tic)
 		{
 			textcmdplayer_t *textcmdplayer = textcmdtic->playercmds[i];
 
-			while (textcmdplayer)
+			while (textcmdplayer > (textcmdplayer_t*)255)  // STAR NOTE: LEAVEBUG HACK
 			{
 				textcmdplayer_t *tcpnext = textcmdplayer->next;
 				Z_Free(textcmdplayer);
@@ -338,7 +337,7 @@ static UINT8* D_GetExistingTextcmd(tic_t tic, INT32 playernum)
 	textcmdtic_t *textcmdtic = textcmds[tic & (TEXTCMD_HASH_SIZE - 1)];
 	while (textcmdtic && textcmdtic->tic != tic) textcmdtic = textcmdtic->next;
 
-	// STAR NOTE: THIS IS THE LEAVEBUG FIX //
+	// STAR NOTE: THIS ENITRE BLOCK HAS BEEN EDITED WITH THE LEAVEBUG FIX //
 	// Do we have an entry for the tic? If so, look for player.
 	if (textcmdtic && textcmdtic->playercmds)
 	{
@@ -349,7 +348,7 @@ static UINT8* D_GetExistingTextcmd(tic_t tic, INT32 playernum)
 		if (textcmdplayer > limit)
 			return textcmdplayer->cmd;
 		else if (textcmdplayer)
-			CONS_Alert(CONS_WARNING, "textcmdplayer is %p: CRASH AVERTED\n", textcmdplayer);
+			CONS_Debug(DBG_NETPLAY, "textcmdplayer is %p: CRASH AVERTED\n", textcmdplayer);
 	}
 
 	return NULL;
