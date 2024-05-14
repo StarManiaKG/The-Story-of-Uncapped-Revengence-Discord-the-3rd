35272ef8d26bc98b22d19e0b36a1cdfbae6d758a
StarManiaKG <DraytonM98@outlook.com>
2024-05-13 20:04:47 -0400

    beginning of the discord revamp

---
+++ src/discord/discord_gamesdk.h
@@ -0,0 +1,63 @@
+// SONIC ROBO BLAST 2; TSOURDT3RD
+//-----------------------------------------------------------------------------
+// Copyright (C) 2023-2024 by Star "Guy Who Names Scripts After Him" ManiaKG.
+//
+// This program is free software distributed under the
+// terms of the GNU General Public License, version 2.
+// See the 'LICENSE' file for more details.
+//-----------------------------------------------------------------------------
+/// \file  discord_gamesdk.h
+/// \brief Discord Game SDK handling
+
+#ifndef _DISCORDGAMESDK__
+#define _DISCORDGAMESDK__
+
+#ifdef HAVE_DISCORDGAMESDK
+
+#include <stdint.h>
+#include <stdio.h>
+#include <assert.h>
+#include "discord_game_sdk.h"
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#include <string.h>
+#endif
+
+extern struct discordGameSDK_s {
+    struct IDiscordCore* core;
+    struct IDiscordUserManager* users;
+    struct IDiscordAchievementManager* achievements;
+    struct IDiscordActivityManager* activities;
+    struct IDiscordRelationshipManager* relationships;
+    struct IDiscordApplicationManager* application;
+    struct IDiscordLobbyManager* lobbies;
+    
+    DiscordUserId user_id;
+} discordGameSDK_t;
+
+#define DISCORD_REQUIRE(x) assert(x == DiscordResult_Ok)
+
+/*--------------------------------------------------
+	void DRPC_Init(int argc, char** argv)
+
+		Initalizes Discord's Game SDK by linking the Application ID
+		and setting the callback functions.
+--------------------------------------------------*/
+
+void DRPC_Init(int argc, char** argv);
+
+/*--------------------------------------------------
+	void DRPC_UpdatePresence(void);
+
+		Updates what is displayed by Game SDK on the user's profile.
+		Should be called whenever something that is displayed is
+		changed in-game.
+--------------------------------------------------*/
+
+void DRPC_UpdatePresence(void);
+
+#endif // HAVE_DISCORDGAMESDK
+#endif // _DISCORDGAMESDK__
