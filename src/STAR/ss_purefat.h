9d4f189aec38be03e40fb506c7ade4f44fdc025b
StarManiaKG <draytonm98@outlook.com>
2024-07-05 17:40:10 +0000

    finalecount is globalized stupid

--- src/STAR/ss_purefat.h
+++ src/STAR/ss_purefat.h
@@ -21,7 +21,7 @@
 
 void STAR_F_StartIntro(tic_t *introtime);
 
-void STAR_F_PureFatDrawer(char *stjrintro, patch_t *background, void *patch, INT32 intro_scenenum, INT32 finalecount, INT32 bgxoffs);
+void STAR_F_PureFatDrawer(char *stjrintro, patch_t *background, void *patch, INT32 intro_scenenum, INT32 bgxoffs);
 void STAR_F_PureFatTicker(INT32 intro_scenenum, INT32 intro_curtime, INT32 animtimer);
 
 #endif // __SS_PUREFAT__
