0e7cf20cbc94247bda08ccec36e348bb2e385b91
StarManiaKG <DraytonM98@outlook.com>
2024-11-24 23:06:04 -0500

    last second lua controller additions

--- src/STAR/lua/smkg-lu_base.c
+++ src/STAR/lua/smkg-lu_base.c
@@ -10,6 +10,7 @@
 /// \brief TSoURDt3rd's base Lua library
 
 #include "smkg-lu_main.h"
+#include "../core/smkg-p_pads.h"
 
 #include "../../lua_libs.h"
 #include "../../d_player.h"
@@ -19,6 +20,78 @@
 // ------------------------ //
 //        Functions
 // ------------------------ //
+
+static int lib_pLEDController(lua_State *L)
+{
+	INT32 player = (UINT8)luaL_checkinteger(L, 1);
+	UINT8 red = (UINT8)luaL_checkinteger(L, 2);
+	UINT8 green = (UINT8)luaL_checkinteger(L, 3);
+	UINT8 blue = (UINT8)luaL_checkinteger(L, 4);
+
+	//NOHUD
+
+	TSoURDt3rd_P_Pads_SetIndicatorColor(player, red, green, blue);
+
+	return 0;
+}
+
+static int lib_pRumbleController(lua_State *L)
+{
+	player_t *player = NULL;
+	fixed_t low = luaL_checkfixed(L, 2);
+	fixed_t high = luaL_checkfixed(L, 3);
+	tic_t duration_tics = luaL_checkinteger(L, 4);
+
+	//NOHUD
+
+	if (!lua_isnone(L, 1) && lua_isuserdata(L, 1))
+		player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
+	TSoURDt3rd_P_Pads_PlayerDeviceRumble(player, low, high, duration_tics);
+
+	return 0;
+}
+
+static int lib_pRumbleControllerTriggers(lua_State *L)
+{
+	player_t *player = NULL;
+	fixed_t left_strength = luaL_checkfixed(L, 2);
+	fixed_t right_strength = luaL_checkfixed(L, 3);
+	tic_t duration_tics = luaL_checkinteger(L, 4);
+
+	//NOHUD
+
+	if (!lua_isnone(L, 1) && lua_isuserdata(L, 1))
+		player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
+	TSoURDt3rd_P_Pads_PlayerDeviceRumbleTriggers(player, left_strength, right_strength, duration_tics);
+
+	return 0;
+}
+
+static int lib_pPauseControllerRumble(lua_State *L)
+{
+	player_t *player = NULL;
+	boolean rumbling_paused = luaL_checkboolean(L, 2);
+	boolean trigger_rumbling_paused = luaL_checkboolean(L, 3);
+
+	//NOHUD
+
+	if (!lua_isnone(L, 1) && lua_isuserdata(L, 1))
+		player = *((player_t **)luaL_checkudata(L, 1, META_PLAYER));
+	TSoURDt3rd_P_Pads_PauseDeviceRumble(player, rumbling_paused, trigger_rumbling_paused);
+
+	return 0;
+}
+
+static int lib_pResetControllerRumble(lua_State *L)
+{
+	INT32 player = luaL_checkinteger(L, 1);
+
+	//NOHUD
+
+	TSoURDt3rd_P_Pads_ResetDeviceRumble(player);
+
+	return 0;
+}
 
 static int lib_sGetSpeedMusic(lua_State *L)
 {
@@ -145,6 +218,12 @@ static int lib_sGetInternalSfxVolume(lua_State *L)
 }
 
 static luaL_Reg tsourdt3d_base_lib[] = {
+	{"P_LEDController",lib_pLEDController},
+	{"P_RumbleController",lib_pRumbleController},
+	{"P_RumbleControllerTriggers",lib_pRumbleControllerTriggers},
+	{"P_PauseControllerRumble",lib_pPauseControllerRumble},
+	{"P_ResetControllerRumble",lib_pResetControllerRumble},
+
 	{"S_GetSpeedMusic",lib_sGetSpeedMusic},
 	{"S_PitchMusic",lib_sPitchMusic},
 	{"S_GetPitchMusic",lib_sGetPitchMusic},
