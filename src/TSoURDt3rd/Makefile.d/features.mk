1e65f370cc0c90e43680a74591e93f56415c2ee0
StarManiaKG <DraytonM98@outlook.com>
2025-06-23 03:58:39 -0400

    sources update

--- src/TSoURDt3rd/Makefile.d/features.mk
+++ src/TSoURDt3rd/Makefile.d/features.mk
@@ -2,33 +2,51 @@
 ## Unique feature flags for TSoURDt3rd
 ##
 
+## MAIN ##
+opts+=-DUSE_STUN
+sources+=$(call List,STAR/Sourcefile)
+sources+=$(call List,STAR/monocypher/Sourcefile)
+
+## LIBS ##
+# Discord #
 ifdef HAVE_DISCORDRPC
-ifndef HAVE_DISCORDGAMESDK
-libs+=-ldiscord-rpc
-opts+=-DHAVE_DISCORDRPC -DUSE_STUN -DHAVE_DISCORDSUPPORT
-sources+=$(call List,discord/Sourcefile)
-else
-$(error \
-	You can't have your cake and eat it too!\
-	Choose either Discord RPC or Discord Game SDK!)
-endif
+  ifndef HAVE_DISCORDGAMESDK
+    DISCORD_SUPPORTED:=1
+    DISCORD_RPC:=1
+    opts+=-DHAVE_DISCORDRPC
+    sources+=$(call List,discord/rpc/Sourcefile)
+  else
+    $(error \
+      You can't have your cake and eat it too!\
+      Choose either Discord RPC or Discord Game SDK!)
+  endif
 endif
 
 ifdef HAVE_DISCORDGAMESDK
-ifndef HAVE_DISCORDRPC # you can't have your cake and eat it too!
-opts+=-DHAVE_DISCORDGAMESDK -DUSE_STUN -DHAVE_DISCORDSUPPORT
-sources+=$(call List,discord/Sourcefile)
-else
-$(error \
-	You can't have your cake and eat it too!\
-	Choose either Discord Game SDK or Discord RPC!)
-endif
+  ifndef HAVE_DISCORDRPC
+    DISCORD_SUPPORTED:=1
+    DISCORD_GAMESDK:=1
+    opts+=-DHAVE_DISCORDGAMESDK
+    sources+=$(call List,discord/gamesdk/Sourcefile)
+  else
+    $(error \
+      You can't have your cake and eat it too!\
+      Choose either Discord Game SDK or Discord RPC!)
+  endif
 endif
 
+ifdef DISCORD_SUPPORTED
+  opts+=-DHAVE_DISCORDSUPPORT
+  sources+=$(call List,discord/Sourcefile)
+endif
+sources+=discord/discord_net.c
+
+# LibAV #
 ifdef HAVE_LIBAV
-libs+=-lm -lz -lavcodec -lavdevice -lavfilter -lavformat -lavresample -lavutil -lswscale
-opts+=-DHAVE_LIBAV
-sources+=$(call List,STAR/libav/Sourcefile)
+  opts+=-DHAVE_LIBAV
+  sources+=$(call List,STAR/libav/Sourcefile)
 endif
 
-sources+=$(call List,STAR/Sourcefile)
+ifdef MINGW
+  include TSoURDt3rd/Makefile.d/win32.mk
+endif
--- src/TSoURDt3rd/Makefile.d/win32.mk
+++ src/TSoURDt3rd/Makefile.d/win32.mk
@@ -3,33 +3,35 @@
 # Except now it also includes cool TSoURDt3rd stuff too :)
 #
 
-ifdef HAVE_DISCORDRPC
-ifdef MINGW64
-opts+=-I../libs/discord-rpc/win64-dynamic/include
-libs+=-L../libs/discord-rpc/win64-dynamic/lib
-else
-opts+=-I../libs/discord-rpc/win32-dynamic/include
-libs+=-L../libs/discord-rpc/win32-dynamic/lib
-endif
-libs+=-ldiscord-rpc
+ifdef DISCORD_RPC
+  ifdef MINGW64
+    opts+=-I../libs/discord-rpc/win64-dynamic/include
+    libs+=-L../libs/discord-rpc/win64-dynamic/lib
+  else
+    opts+=-I../libs/discord-rpc/win32-dynamic/include
+    libs+=-L../libs/discord-rpc/win32-dynamic/lib
+  endif
+  libs+=-ldiscord-rpc
 endif
 
-ifdef HAVE_DISCORDGAMESDK
-opts+=-I../libs/discord-game-sdk/include
-ifdef MINGW64
-libs+=-L../libs/discord-game-sdk/x86_64
-else
-libs+=-L../libs/discord-game-sdk/x86
-endif
+ifdef DISCORD_GAMESDK
+  opts+=-I../libs/discord_game_sdk/include
+  ifdef MINGW64
+    libs+=-L../libs/discord_game_sdk/x86_64
+  else
+    libs+=-L../libs/discord_game_sdk/x86
+  endif
+  libs+=-ldiscord_game_sdk
 endif
 
 ifdef HAVE_LIBAV
-ifdef MINGW64
-opts+=-I../libs/libav/x86_64-w64-mingw32/include
-libs+=-L../libs/libav/x86_64-w64-mingw32/lib
-else
-opts+=-I../libs/libav/i686-w64-mingw32/include
-libs+=-L../libs/libav/i686-w64-mingw32/lib
-endif
-libs+=-lvfw32 -lws2_32 -lbcrypt -luser32
+  ifdef MINGW64
+    opts+=-I../libs/libav/x86_64-w64-mingw32/include
+    libs+=-L../libs/libav/x86_64-w64-mingw32/lib
+  else
+    opts+=-I../libs/libav/i686-w64-mingw32/include
+    libs+=-L../libs/libav/i686-w64-mingw32/lib
+  endif
+  libs+=-lvfw32 -lws2_32 -lbcrypt -luser32
+  libs+=-lm -lz -lavcodec -lavdevice -lavfilter -lavformat -lavresample -lavutil -lswscale
 endif
